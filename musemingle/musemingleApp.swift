bdc234b1d6b24f68b5582062b88380908251659e
JAMIEPARK90 <jamiebhpark@gmail.com>
2023-08-17 19:47:49 +0900

    Initial Commit

---
+++ musemingle/musemingleApp.swift
@@ -0,0 +1,17 @@
+//
+//  musemingleApp.swift
+//  musemingle
+//
+//  Created by JONGHUN PARK on 2023/08/17.
+//
+
+import SwiftUI
+
+@main
+struct musemingleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
