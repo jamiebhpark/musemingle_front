c063d4d52367b191e19edc7182041bd073383db8
jamiebhpark <67772599+jamiebhpark@users.noreply.github.com>
2023-11-01 19:27:33 +0900

    Update SignInOrSignUpView.swift
    11.01 updated

--- musemingle/SignInOrSignUpView.swift
+++ musemingle/SignInOrSignUpView.swift
@@ -1,53 +1,89 @@
 import SwiftUI
 
-struct WelcomeView: View {
-    private var viewModel: AppViewModel
-    @StateObject private var loginStatusViewModel: LoginStatusViewModel
-    @StateObject private var appleSignInManager: AppleSignInManager
+class SignInOrSignUpViewModel: ObservableObject {
+    @Published var isLoggedIn = false
+    @Published var selectedTab: CustomTabBar.Tab = .home
+}
 
+struct SignInOrSignUpView: View {
+    @ObservedObject private var viewModel: AppViewModel
+    private var loginStatusViewModel: LoginStatusViewModel
+    
     init(viewModel: AppViewModel, loginStatus: LoginStatusViewModel) {
         self.viewModel = viewModel
-        self._loginStatusViewModel = StateObject(wrappedValue: loginStatus)
-        self._appleSignInManager = StateObject(wrappedValue: AppleSignInManager(loginStatusViewModel: loginStatus))
+        self.loginStatusViewModel = loginStatus
     }
+    
     var body: some View {
-
-
-        NavigationView {
-            ZStack {
-                Image("bgimage")
-
-                    .resizable()
-                    .scaledToFill()
-                    .edgesIgnoringSafeArea(.all)
-
-                VStack(spacing: 20) {
-                    Spacer()
-
-                    NavigationLink(destination: SignInOrSignUpView(viewModel: viewModel, loginStatus: loginStatusViewModel)) {
-
-                        Text("Create an account")
-                            .padding(.vertical, 12)
-                            .frame(maxWidth: 300)
-                            .foregroundColor(.white)
-                            .background(Color.blue)
-                            .cornerRadius(10)
+        ZStack {
+            // 배경 이미지 추가
+            Image("bgimage")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Spacer()
+                Button(action: {
+                    viewModel.isLoggedIn = true
+                }) {
+                    HStack {
+                        Image("google_icon")
+                            .resizable()
+                            .frame(width: 18, height: 18)
+                        Text("Continue with Google")
                     }
-                    .padding([.leading, .trailing], 16)
-
-                    NavigationLink(destination: SignInOrSignUpView(viewModel: viewModel, loginStatus: loginStatusViewModel)){ // Fixed here
-                        Text("I already have an account")
-                            .padding(.vertical, 12)
-                            .frame(maxWidth: 300)
-                            .foregroundColor(.blue)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(Color.blue, lineWidth: 1)
-                            )
-                    }
-                    .padding([.leading, .trailing], 16)
+                    .padding(.vertical, 12)
+                    .frame(maxWidth: 280)
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .foregroundColor(.black)
+                    .shadow(color: .black.opacity(0.25), radius: 1, x: 0, y: 1)
+                    .shadow(color: .black.opacity(0.08), radius: 0, x: 0, y: 0)
                 }
+                .padding(.bottom, 10)
+                
+                Button(action: {
+                    viewModel.isLoggedIn = true
+                }) {
+                    HStack {
+                        Image("facebook_icon")
+                            .resizable()
+                            .frame(width: 18, height: 18)
+                        Text("Continue with Facebook")
+                    }
+                    .padding(.vertical, 12)
+                    .frame(maxWidth: 280)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    .foregroundColor(.white)
+                }
+                .padding(.bottom, 10)
+                
+                Button(action: {
+                    viewModel.isLoggedIn = true
+                }) {
+                    HStack {
+                        Image(systemName: "applelogo")
+                            .resizable()
+                            .frame(width: 18, height: 18)
+                        Text("Continue with Apple")
+                    }
+                    .padding(.vertical, 12)
+                    .frame(maxWidth: 280)
+                    .background(Color.black)
+                    .cornerRadius(10)
+                    .foregroundColor(.white)
+                }
+                .padding(.bottom, 50)
             }
+            .padding([.leading, .trailing], 16)
+            .navigationBarHidden(true)
+            .overlay(
+                NavigationLink(destination: MainView().environmentObject(viewModel), isActive: $viewModel.isLoggedIn) {
+                    EmptyView()
+                }
+                    .hidden()
+            )
         }
     }
 }
